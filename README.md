39bd6f0474c50d040ddd1f534780e97cb75cb4b7
iitalics <iitalics@gmail.com>
2016-10-15 17:35:52 -0400

    added link to preview

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # MakawaoWeather
-Interactive graph of weather data in upcountry Maui 
+Interactive graph of weather data in upcountry Maui
+Preview on [jsfiddle.net](http://jsfiddle.net/qr7kr745/embedded/result,js,html/)
 
 ## Dependencies
     - Uses [httpserver](https://github.com/indexzero/http-server) to server pages
---
+++ gen-hardcoded.js
@@ -0,0 +1,51 @@
+/* this script is used to put the JSON data and weather.js
+ * in one file, so that it can be shared on sites such as jsfiddle.net */
+let fs = require("fs")
+  , process = require("process");
+
+/* files and stuff */
+let output = process.stdout;
+let root = "./public";
+let dataDir = "data";
+let basicScript = "weather.js";
+let destVar = "hardcoded";
+
+fs.readdir(root + "/" + dataDir, (err, list) => {
+  if (err)
+    return console.error(err);
+
+  let entries = {};
+  let n = 0;
+
+  for (let filename of list) {
+    let path = root + "/" + dataDir + "/" + filename;
+    if (filename.match(/[^\.]\.json$/)) {
+      n++;
+      fs.readFile(path, (err,data) => {
+        entries[filename.match(/(.+)\.json$/)[1]] = data;
+        read_one();
+      });
+    }
+  }
+
+  function read_one () {
+    if (--n === 0) {
+      finish(entries);
+    }
+  }
+});
+
+function finish (entries) {
+  fs.readFile(root + "/" + basicScript, (err,data) => {
+    if (err)
+      return console.error(err);
+
+    output.write(data);
+    output.write("\n\n\n/*** JSON data pulled from files ***/\n");
+    output.write(destVar + " = {\n");
+    for (let key of Object.keys(entries)) {
+      output.write("  " + key + ": " + entries[key] + ",\n");
+    }
+    output.write("}\n");
+  });
+}
